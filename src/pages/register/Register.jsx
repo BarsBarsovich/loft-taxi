e0b6b6988aefd89cfe7f11b42559a56b921fd870
Boris Dryabin <b.dryabin@rt-solar.ru>
2021-04-06 21:39:14 +0300

    add state

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,5 @@
 import Button from "../../components/button/Button";
-import React from "react";
+import React, {useState} from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Input from "../../components/input/Input";
 import './Register.css';
@@ -7,6 +7,9 @@ import {Link} from "react-router-dom";
 
 
 function Register() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <div className='wrapper'>
             <Sidebar/>
@@ -15,10 +18,10 @@ function Register() {
                     <div className="login__container">
                         <p className='login-title'>Регистрация</p>
                         <form className='login-form'>
-                            <Input title='Email*' type='text' placeholder='mail@mail.ru' value=''/>
-                            <Input title='Как вас зовут?*' type='text' placeholder='Петр Александрович' value=''/>
+                            <Input title='Email*' type='text' placeholder='mail@mail.ru' value={email} onChange={setEmail} />
+                            <Input title='Как вас зовут?*' type='text' placeholder='Петр Александрович' value={name} onChange={setName}/>
 
-                            <Input title='Придумайте пароль*' type='password' placeholder='Введите пароль' value=''/>
+                            <Input title='Придумайте пароль*' type='password' placeholder='Введите пароль' value={password} onChange={setPassword}/>
                             <Button title='Войти' type='submit' className='submit'/>
                         </form>
                         <div className="login__footer">
